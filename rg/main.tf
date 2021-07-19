f4efce85237732d335f71608e4a7ad2f116062f2
Harishankar4274 <haishankardubey2911@gmail.com>
2021-07-19 22:49:16 +0530

    update .gitignore

--- rg/main.tf
+++ rg/main.tf
@@ -1,4 +1,4 @@
-resource "azure_resource_group" "az_rg_name" {
+resource "azurerm_resource_group" "az_rg_name" {
   name = var.az_rg_name
   location = var.az_rg_reg
 }
---
+++ rg/output.tf
@@ -0,0 +1,3 @@
+output "az_rg_name" {
+  value = azurerm_resource_group.az_rg_name.name
+}
